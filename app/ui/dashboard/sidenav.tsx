42830bcb03fe28fa3a6f9344b3ca97d5a9779f47
Volkan <ugurvolkangul@gmail.com>
2023-11-08 08:16:11 -0800

    Chapter 5

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "./nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
 export default function SideNav() {
   return (
